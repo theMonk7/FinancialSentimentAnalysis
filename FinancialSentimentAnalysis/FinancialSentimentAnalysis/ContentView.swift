6d043e18af693fa0af82221402fbac51e78d5391
utkarshraj <utkarshraj@urbancompany.com>
2024-11-02 15:12:41 +0530

    Initial commit

---
+++ FinancialSentimentAnalysis/FinancialSentimentAnalysis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinancialSentimentAnalysis
+//
+//  Created by Utkarsh Raj on 02/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinancialSentimentAnalysis/FinancialSentimentAnalysis/FinancialSentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialSentimentAnalysisApp.swift
+//  FinancialSentimentAnalysis
+//
+//  Created by Utkarsh Raj on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FinancialSentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
