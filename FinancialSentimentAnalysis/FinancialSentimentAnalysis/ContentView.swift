7fd726d4fff08684758c53bd01d924dbd4ce0cd9
utkarshraj <utkarshraj@urbancompany.com>
2024-11-02 16:27:10 +0530

    Add model and create text inputs

--- FinancialSentimentAnalysis/FinancialSentimentAnalysis/ContentView.swift
+++ FinancialSentimentAnalysis/FinancialSentimentAnalysis/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import CoreML
+
 
 struct ContentView: View {
+    @State private var text = ""
+    @State private var result = ""
+    let model = try! SentimentModel(configuration: MLModelConfiguration())
+    
+    private func sentimentColorMap(sentiment: String) -> Color {
+        switch sentiment {
+        case "positive":
+            return .green
+        case "negative":
+            return .red
+        default:
+            return .white
+        }
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            TextField(text: $text) {
+                Text("Enter the text")
+            }
+            HStack {
+                Text("Sentiment")
+                Spacer()
+                Text(result)
+            }
+            Spacer()
+            Button(action: {
+                do {
+                    result = try model.prediction(text: text).label
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            }, label: {
+                Text("Calculate")
+            })
+            .buttonStyle(.borderedProminent)
+            
+            
         }
         .padding()
+        .background(sentimentColorMap(sentiment: result))
     }
 }
 
